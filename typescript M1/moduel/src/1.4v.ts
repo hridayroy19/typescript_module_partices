f756f5b533c706832e72cf5c64eae6761eb2962b
hridayroy19 <hrhridoyroy503@gmail.com>
2024-10-30 20:50:41 +0600

    first commit

---
+++ typescript M1/moduel/src/1.4v.ts
@@ -0,0 +1,28 @@
+// basic data types  
+
+// string 
+let fastName :string = "Hridoy"
+
+// number
+let roll :number = 503319
+
+// boolean
+let isAdmin : boolean = true
+
+// null
+let x : null = null
+ 
+// undefiend 
+let y : undefined = undefined
+
+// anytype .. basicaly i dont use any type nhel typesctip er power bujte parbo nh
+
+ let Hridoy;  
+
+//  arry
+
+let friends : string [] = ["toma","sathi"]
+let number : number[] = [ 1,2,3]
+
+// tuple --> arry - order ->type of value
+ let ageName : [number, string, boolean] = [25,"hridoy", true]
