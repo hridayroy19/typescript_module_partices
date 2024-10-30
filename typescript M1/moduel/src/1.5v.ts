77e512c915a35956cb08664700d290c39194457d
hridayroy19 <hrhridoyroy503@gmail.com>
2024-10-30 21:18:24 +0600

    learing referance type objcet

---
+++ typescript M1/moduel/src/1.5v.ts
@@ -0,0 +1,16 @@
+// referance type  --> objcet
+
+const user: {
+    fastName: string;
+    medilName?: string;
+    lastName: string;
+    company: "programing Hero";  //sobsomay fiexd thakbe
+    isSingle : boolean
+} = {
+    company: "programing Hero", //literal type > same rakhte hbe 
+    //company: "programing Hero Bangladesh",
+       fastName: "Hriody",
+    // medilName :"incoming",
+    lastName: "roy",
+    isSingle : true
+}
