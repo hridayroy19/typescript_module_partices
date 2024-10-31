44454be31a7d8d21090eedbca4dc18ca62c61630
hridayroy19 <hrhridoyroy503@gmail.com>
2024-10-31 09:28:10 +0600

    updte readme file

---
+++ typescript M1/moduel/src/1.6v.js
@@ -0,0 +1,8 @@
+// learning function
+// normal fulciotn
+// arrow function
+function add(num1, num2) {
+    return num1 + num2;
+}
+var result = add(2, 4);
+console.log(result);
---
+++ typescript M1/moduel/src/1.6v.ts
@@ -0,0 +1,12 @@
+// learning function
+// normal fulciotn
+// arrow function
+
+function add(num1: number, num2: number = 15 ): number {
+    return num1 + num2;
+}
+
+const result = add(2, 4);
+// console.log(result);
+
+const addarrow = ( num1:number , num2:number):number => num1 + num2
