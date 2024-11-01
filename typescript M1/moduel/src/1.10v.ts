2cb8a25fb472f56fa40d703322f2ef8db26cd514
hridayroy19 <hrhridoyroy503@gmail.com>
2024-11-01 09:47:24 +0600

    ternary operator

---
+++ typescript M1/moduel/src/1.10v.ts
@@ -0,0 +1,30 @@
+{
+    // ternary operator || optinal chaning || nulish coalesing operator
+
+
+    const age : number = 25;
+
+    if(age >= 25){
+        console.log("adult");
+        
+    } else{
+        console.log("not adult");
+        
+    }
+
+
+     const isAdult = age >=18 ? "adult" : "not adult";
+     console.log({isAdult});
+     
+
+
+
+
+
+
+
+
+
+
+
+}
