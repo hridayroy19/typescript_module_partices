bbcb27a925da383a96e0a077e31c2cd5072c2bab
hridayroy19 <hrhridoyroy503@gmail.com>
2024-11-01 18:40:29 +0600

    type assertion

---
+++ module 2/2.1v.ts
@@ -0,0 +1,13 @@
+{
+    // type assertion
+
+    // tumi amr thake besi bujo amn kichu mane ami besi buji > kuno kichu sure hoye kaj kora amn kichu jeta ami developer hisabe type script er thake besi buji
+
+    let anything: any;
+
+    anything = "next level web developer";     //what is anything type dont understand typescript but assertion kaj holo kno kichu sure hye neoya                         
+    //   anything = 12313
+
+    (anything as string)
+
+}
