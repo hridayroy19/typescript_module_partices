4d9f7406721f12b373df7784641484d491c98036
hridayroy19 <hrhridoyroy503@gmail.com>
2024-11-02 11:41:36 +0600

    fuction with generic

---
+++ module 2/2.5v.ts
@@ -0,0 +1,28 @@
+{
+    //fuction with generices
+
+      const creteeArray = (param : string) : string[]=>{
+        return [param]
+      }
+
+
+      const creteeArrayWith = <T> (param : T) : T[]=>{
+        return [param]
+      }
+
+     const res1 = creteeArrayWith<string>("bangladesh");
+     const res2 = creteeArrayWith<boolean>(true)
+
+// <><><><>
+      interface user {
+        id : number,
+        name : string
+      }
+  const resgenarice = creteeArrayWith<user>({
+    id:1212,
+    name:"hello "
+  })
+
+
+    //
+}
