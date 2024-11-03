f2959c260add298e51e00c1bf3473df1b9311095
hridayroy19 <hrhridoyroy503@gmail.com>
2024-11-03 18:24:13 +0600

    mapped

---
+++ module 2/2.10v.ts
@@ -0,0 +1,27 @@
+{
+    //mapped types 
+
+
+   const nus: number[] = [1,2,3,4,5];
+
+   const mapingNumber: string[] = nus.map((nmb) =>
+     nmb.toString()
+   );
+
+ console.log(mapingNumber);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+//
+
+}
