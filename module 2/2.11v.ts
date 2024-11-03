9fea99e9fdef9a3dba8392c9b1316c6ff0c84995
hridayroy19 <hrhridoyroy503@gmail.com>
2024-11-03 18:53:14 +0600

    pick learming

---
+++ module 2/2.11v.ts
@@ -0,0 +1,28 @@
+{
+    // utility types
+    //pick
+
+
+    type person = {
+        name:string;
+        age:number;
+        addresh:string
+        location:string
+    }
+
+type nameAge = Pick< person , "name" | 'age'>   //pick er madhorm amra kono spacifice data niye aste pari
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //
+}
