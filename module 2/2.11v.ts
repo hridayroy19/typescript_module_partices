869e0c0b0e0a89837891aa909e690f4976fdbfa3
hridayroy19 <hrhridoyroy503@gmail.com>
2024-11-03 19:02:13 +0600

    omit usering

--- module 2/2.11v.ts
+++ module 2/2.11v.ts
@@ -13,10 +13,7 @@
 type nameAge = Pick< person , "name" | 'age'>   //pick er madhorm amra kono spacifice data niye aste pari
 
 
-
-
-
-
+type personContactInfo = Omit <person,"name"|"age">  // type এ কেবল name এবং age property থাকবে, কারণ location এবং address বাদ দেওয়া হয়েছে। Omit এর মাধ্যমে আমরা সহজেই নির্দিষ্ট কিছু property বাদ দিয়ে একটি নতুন type তৈরি করতে পারছি।
 
 
 
